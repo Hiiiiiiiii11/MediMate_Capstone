bd6509335410d0313b6cd7f900de110d4081c724
Quoc Huy <hhh1z1369@gmail.com>
2026-03-03 21:59:38 +0700

    Add auth, medication, and migration updates
    - Added `[Authorize]` and `Logout` endpoint in `AuthController.cs`.
    - Improved error handling in `HealthController.cs` with `try-catch`.
    - Introduced `MedicationSchedulesController` for schedule management.
    - Updated `Program.cs` for DI, Firebase, and Hangfire setup.
    - Refactored database context initialization in `Program.cs`.
    - Enhanced Swagger and middleware setup for Hangfire dashboard.
    - Added migration for new entities and schema changes:
      - Added `FcmToken` to `Users` and `Members`.
      - Added `EndTime` to `MedicationReminders`.
    - Updated `MediMateDbContextModelSnapshot.cs` for schema changes.
    - Improved code readability, maintainability, and security.

---
+++ MediMateRepository/Migrations/20260226163402_addatributeFmc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediMateRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addatributeFmc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FcmToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FcmToken",
+                table: "Members",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "MedicationReminders",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FcmToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FcmToken",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "MedicationReminders");
+        }
+    }
+}

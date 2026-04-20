5d17bef378f555d31ccd3a361aecef73d32bb886
Quoc Huy <hhh1z1369@gmail.com>
2026-04-21 02:46:24 +0700

    add migration

---
+++ MediMateRepository/Migrations/20260420194540_addStatusException1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediMateRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addStatusException1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "DoctorAvailabilityExceptions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "DoctorAvailabilityExceptions",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}

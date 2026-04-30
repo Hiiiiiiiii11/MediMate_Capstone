de7910195e216b858ae98adf2a942327978441dd
SangHamChoi <sangdnse184169@fpt.edu.vn>
2026-04-30 19:57:41 +0700

    Update transaction types for package and session payments
    Changed transaction type from 'MoneyReceived' to new types
    'InPackagePurchase' and 'InSessionPayment' in PayOSService.
    Added corresponding constants to TransactionTypes for clarity.

--- Share/Constants/TransactionTypes.cs
+++ Share/Constants/TransactionTypes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,8 @@ namespace Share.Constants
     public static class TransactionTypes
     {
         public const string MoneyReceived = "IN";
+        public const string InPackagePurchase = "IN_PACKAGE";
+        public const string InSessionPayment = "IN_SESSION";
         public const string MoneySent = "OUT";
     };
 }
